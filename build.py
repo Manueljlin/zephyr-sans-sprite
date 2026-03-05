4617e9bf609d53b7c3451296e014e3407ea0029f
Manuel Jesus de la Fuente Pereira <m@nueljl.in>
2026-03-05 23:43:03 +0100

    upd ~

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
 import subprocess
+import sys
 from pathlib import Path
 
 UFO = Path(__file__).parent / "ZephyrSansSprite.ufo"
 OUT = Path(__file__).parent / "output"
 OUT.mkdir(exist_ok=True)
 
+fontmake = Path(sys.executable).parent / "fontmake"
+
 subprocess.run([
-    "fontmake",
+    str(fontmake),
     "-u", str(UFO),
     "-o", "ttf", "otf",
     "--output-dir", str(OUT),
